991c12d035e7dcc1fd9f0e382676217269908187
Kwangnam Yu <53140105+phykn@users.noreply.github.com>
2022-06-06 18:15:27 +0900

    v0.0.4

--- setup.py
+++ setup.py
@@ -1,27 +1,18 @@
 import setuptools
 
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-
 setuptools.setup(
     name="smmo",
-    version="0.0.2",
+    version="0.0.1",
+    license='MIT',
     author="Kwangnam Yu",
     author_email="phykn.kr@gmail.com",
     description="Scattering-Matrix method for multilayer optics",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
     url="https://github.com/phykn/smmo",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
     ],
-    python_requires='>=3.6',
-    install_requires=[
-        "numpy"
-    ]
 )
