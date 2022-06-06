89ab85bdb9d036b121b941242ac632d4beb811c1
Kwangnam Yu <53140105+phykn@users.noreply.github.com>
2022-06-06 16:47:20 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="smmo",
+    version="0.0.2",
+    author="Kwangnam Yu",
+    author_email="phykn.kr@gmail.com",
+    description="Scattering-Matrix method for multilayer optics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phykn/smmo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy"
+    ]
+)
